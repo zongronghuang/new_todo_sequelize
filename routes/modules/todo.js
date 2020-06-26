b1c9dad4d8f9590a7c2d33878d777cff0b50a8d1
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-06-27 02:07:49 +0800

    feat: add todo edit routes

--- routes/modules/todo.js
+++ routes/modules/todo.js
@@ -17,12 +17,40 @@ router.post('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
-
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
+
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      res.render('edit', { todo: todo.get() })
+    })
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => {
+      res.redirect(`/todos/${id}`)
+    })
+    .catch(error => console.log(error))
+})
+
 
 module.exports = router
