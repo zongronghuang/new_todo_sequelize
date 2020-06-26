d74fd28bc2b9299ebc57e4bfdabb92481981946b
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-06-27 02:47:09 +0800

    feat: add partials template for showing messages

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -22,6 +22,25 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位皆必填' })
+  }
+
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼不一致' })
+  }
+
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
 
   User.findOne({ where: { email } })
     .then(user => {
